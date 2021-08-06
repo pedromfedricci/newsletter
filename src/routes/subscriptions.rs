5241bc4b2761725f1b5a5cf8252d4906bef429bf
Pedro de Matos Fedricci <pedromfedricci@gmail.com>
2021-08-06 05:15:23 +0000

    Serialize FormData fildes names

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,8 +2,10 @@ use actix_web::{web::Form, HttpResponse};
 
 #[derive(serde::Deserialize)]
 pub(crate) struct FormData {
-    email: String,
-    name: String,
+    #[serde(rename(serialize = "email", deserialize = "email"))]
+    _email: String,
+    #[serde(rename(serialize = "name", deserialize = "name"))]
+    _name: String,
 }
 
 pub(crate) async fn subscribe(_req: Form<FormData>) -> HttpResponse {
