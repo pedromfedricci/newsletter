f199c9f113e6056ef98bf8e91b4b996b50dddf0b
Pedro de Matos Fedricci <pedromfedricci@gmail.com>
2021-08-17 02:07:26 +0000

    Add workaround lint flag for clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+// temporarly workaround for clippy incorrect
+// lint at crate::routes::subscription::subscribe
+#![allow(clippy::async_yields_async)]
+
 pub mod config;
 mod routes;
 pub mod startup;
