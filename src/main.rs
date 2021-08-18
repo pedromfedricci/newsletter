bd6fde65d1ee9adc917c4226fba791c5f1aa212c
Pedro de Matos Fedricci <pedromfedricci@gmail.com>
2021-08-18 02:00:34 +0000

    Add sqlx query metadata for offline verification

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use libnewsletter::{config, startup, telemetry};
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 
 #[actix_web::main]
@@ -9,7 +9,9 @@ async fn main() -> std::io::Result<()> {
 
     let config = config::settings().expect("Failed to read configuration");
     let listener = TcpListener::bind(&config.application)?;
-    let db_pool = PgPool::connect_with(config.database.connection_with_db())
+    let db_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(10))
+        .connect_with(config.database.connection_with_db())
         .await
         .expect("Failed to connect to database");
 
